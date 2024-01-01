7b0f423bc13ec961a4a667b2a95c9a4d3c0672ea
frankovo <frank@osehfrank.com>
2024-01-01 18:03:57 +0100

    update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -138,3 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
